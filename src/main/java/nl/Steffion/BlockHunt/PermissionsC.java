e289e6a01f98b8a670015df8d10bce1a13479f5a
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2020-04-14 11:57:57 +0200

    Clean up code

--- src/main/java/nl/Steffion/BlockHunt/PermissionsC.java
+++ src/main/java/nl/Steffion/BlockHunt/PermissionsC.java
@@ -1,17 +1,17 @@
 package nl.Steffion.BlockHunt;
+
 /**
  * Steffion's Engine - Made by Steffion.
  *
  * You're allowed to use this engine for own usage, you're not allowed to
- * republish the engine. Using this for your own plugin is allowed when a
- * credit is placed somewhere in the plugin.
+ * republish the engine. Using this for your own plugin is allowed when a credit
+ * is placed somewhere in the plugin.
  *
  * Thanks for your cooperate!
  *
  * @author Steffion
  */
 public class PermissionsC {
-
 
 	public static String main = BlockHunt.pdfFile.getName().toLowerCase() + ".";
 
@@ -20,10 +20,12 @@ public class PermissionsC {
 	}
 
 	public enum Permissions {
-		info("info", PType.ALL), help("help", PType.ALL), reload("reload", PType.ADMIN), join("join", PType.PLAYER), joinfull("joinfull", PType.MODERATOR), joinsign(
-				"joinsign", PType.PLAYER), leave("leave", PType.PLAYER), list("list", PType.PLAYER), shop("shop", PType.PLAYER), shopblockchooser("shop.blockchooser",
-				PType.ADMIN), start("start", PType.MODERATOR), create("create", PType.ADMIN), set("set", PType.MODERATOR), setwarp("setwarp", PType.MODERATOR), signcreate(
-				"signcreate", PType.MODERATOR), remove("remove", PType.ADMIN), tokens("tokens", PType.ADMIN), allcommands("allcommands", PType.OP);
+		info("info", PType.ALL), help("help", PType.ALL), reload("reload", PType.ADMIN), join("join", PType.PLAYER),
+		joinfull("joinfull", PType.MODERATOR), joinsign("joinsign", PType.PLAYER), leave("leave", PType.PLAYER),
+		list("list", PType.PLAYER), shop("shop", PType.PLAYER), shopblockchooser("shop.blockchooser", PType.ADMIN),
+		start("start", PType.MODERATOR), create("create", PType.ADMIN), set("set", PType.MODERATOR),
+		setwarp("setwarp", PType.MODERATOR), signcreate("signcreate", PType.MODERATOR), remove("remove", PType.ADMIN),
+		tokens("tokens", PType.ADMIN), allcommands("allcommands", PType.OP);
 
 		public String perm;
 		public PType type;
